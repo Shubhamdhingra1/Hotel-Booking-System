d37f1828eab9a83863ad6cb083a2ce2389eca574
Shubham <shubhamdhingra9896@gmail.com>
2025-06-25 16:06:02 +0530

    added some changes

--- admin/src/components/GoogleAuth/googleAuth.js
+++ admin/src/components/GoogleAuth/googleAuth.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD93YcSwcR8UHqi6xuNFzeHSyjoWC2puc8",
-    authDomain: "hotelbook-b6654.firebaseapp.com",
-    projectId: "hotelbook-b6654",
-    storageBucket: "hotelbook-b6654.appspot.com",
-    messagingSenderId: "895726606639",
-    appId: "1:895726606639:web:2bc79947034c57e84b69da",
-    measurementId: "G-5W61FVWB22"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
